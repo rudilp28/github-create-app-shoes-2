ecae630bcebc176fd52edf47e00956af33d93e8e
Rudinei De barros <rudinei.lopes@hotmail.com>
2020-08-24 23:12:09 -0300

    Updates

---
+++ precache-manifest.9dc5b192fb36759ce89885bb57fcd075.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52a10b424e525d5bcfa13a1f2ea588e0",
+    "url": "/github-create-app-shoes-2/index.html"
+  },
+  {
+    "revision": "b11ed326ea061fe0a1fb",
+    "url": "/github-create-app-shoes-2/static/css/main.c3d207b2.chunk.css"
+  },
+  {
+    "revision": "e5c87c64346978ddffb7",
+    "url": "/github-create-app-shoes-2/static/js/2.434dabed.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/github-create-app-shoes-2/static/js/2.434dabed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b11ed326ea061fe0a1fb",
+    "url": "/github-create-app-shoes-2/static/js/main.79918647.chunk.js"
+  },
+  {
+    "revision": "f71b0a2cd25097d0f3ab",
+    "url": "/github-create-app-shoes-2/static/js/runtime-main.7730b7c1.js"
+  }
+]);
